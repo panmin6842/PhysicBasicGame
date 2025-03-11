d328601a826bc28c2a72a9113bfcfff05d9fa484
panmin <panmin6842@naver.com>
2025-03-11 21:10:08 +0900

    Initial commit

---
+++ Assets/Scripts/CircleObject.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleObject : MonoBehaviour
+{
+    public bool isDrag; //드래그 상태 확인
+    Rigidbody2D rigid;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        isDrag = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDrag)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //마우스 위치(스크린 좌표계로)
+
+            //x축 경계 설정
+            float leftBorder = -4.2f + transform.localScale.x / 2f;
+            float rightBorder = 4.2f - transform.localScale.x / 2f;
+
+            //값 고정
+            if (mousePos.x < leftBorder)
+            {
+                mousePos.x = leftBorder;
+            }
+            else if (mousePos.x > rightBorder)
+            {
+                mousePos.x = rightBorder;
+            }
+            mousePos.y = 8;
+            mousePos.z = 0;
+
+            transform.position = Vector3.Lerp(transform.position, mousePos, 0.1f); //위치 마우스 위치로 따라감
+        }
+    }
+
+    public void Drag()
+    {
+        isDrag = true;
+    }
+
+    public void Drop()
+    {
+        isDrag = false;
+        rigid.simulated = true;
+    }
+}
